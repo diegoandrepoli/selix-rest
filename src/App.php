3e046bc2d643c3e684045a58fc0367e21f56a42f
root <root@localhost.localdomain>
2017-01-21 12:29:32 -0200

    Separado lógica da aplicação da raiz principal do projeto

--- src/App.php
+++ src/App.php
@@ -1,6 +1,4 @@
 <?php	
-	require 'vendor/autoload.php';
-	
 	$app = new Silex\Application();
 	
 	/**
---
+++ web/index.php
@@ -0,0 +1,12 @@
+<?php
+
+	/**
+	 * Vendor resources
+	 */
+	require_once __DIR__ . '/../vendor/autoload.php';
+
+	/**
+	 * Application
+	 */
+	require __DIR__ . '/../src/App.php';
+	
